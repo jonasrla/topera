e18daef816229bd339c5a4c9cb6361a8d89e48d1
matyok <matyok@uol.com.br>
2014-11-22 18:26:53 -0500

    Create index.py
    frontend lab3

---
+++ index.py
@@ -0,0 +1,16 @@
+from bottle import route, run, template, get, request, redirect, app, hook
+import operator
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import flow_from_clientsecrets
+from apiclient.errors import HttpError
+from apiclient.discovery import build
+import httplib2
+from beaker.middleware import SessionMiddleware
+
+@route('/')
+def index():
+	return("templates/index.html")
+
+@route('/result', method=['GET'])
+def result():
+	
