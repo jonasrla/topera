6288a285d4b4a0955206c767c47c58a9d9bd90d2
Jonas Rocha Lima Amaro <jonasrla@gmail.com>
2014-11-30 17:37:01 -0500

    Create a security groups only if it doesn't exists

--- Backend/aws_handler.py
+++ Backend/aws_handler.py
@@ -11,11 +11,18 @@ f.close()
 connection = boto.ec2.connect_to_region('us-east-1', aws_access_key_id=key[1], aws_secret_access_key=key[2])
 # key = connection.create_key_pair('topera')
 # key.save(".")
-sec_group = connection.create_security_group('csc326-group30','group for users of the search engine Topera')
+sec_group = None
+try:
+	sec_group = connection.create_security_group('csc326-group30','group for users of the search engine Topera')
 
-connection.authorize_security_group(group_name='csc326-group30', ip_protocol='ICMP', from_port=-1, to_port=-1, cidr_ip='0.0.0.0/0')
-connection.authorize_security_group(group_name='csc326-group30', ip_protocol='TCP', from_port=22, to_port=22, cidr_ip='0.0.0.0/0')
-connection.authorize_security_group(group_name='csc326-group30', ip_protocol='TCP', from_port=80, to_port=80, cidr_ip='0.0.0.0/0')
+
+	connection.authorize_security_group(group_name='csc326-group30', ip_protocol='ICMP', from_port=-1, to_port=-1, cidr_ip='0.0.0.0/0')
+	connection.authorize_security_group(group_name='csc326-group30', ip_protocol='TCP', from_port=22, to_port=22, cidr_ip='0.0.0.0/0')
+	connection.authorize_security_group(group_name='csc326-group30', ip_protocol='TCP', from_port=80, to_port=80, cidr_ip='0.0.0.0/0')
+except boto.exception.EC2ResponseError:
+	sec_group = 'csc326-group30'
+	print "Already exist"
+
 res = connection.run_instances('ami-8caa1ce4', key_name="topera", security_groups=["csc326-group30"])
 ins = res.instances[0]
 while ins.state != unicode('running'):
