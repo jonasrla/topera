5c7a6d45161edff07cec523f1dca577859c3e3b9
matyok <matyok@uol.com.br>
2014-12-03 15:55:49 -0500

    Update aws_handler.py

--- Backend/aws_handler.py
+++ Backend/aws_handler.py
@@ -7,9 +7,8 @@ from scp import SCPClient
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file.zip", help="Put the name of the .zip if extention file containing the complete webserver\nIt must contain:\n1. topera.py\n2. data/\n3. database/\n4. static/\n5. templates/")
-file_name = parser.parse_args()
-
+parser.add_argument("file", help="Put the name of the .zip if extention file containing the complete webserver\nIt must contain:\n1. topera.py\n2. data/\n3. database/\n4. static/\n5. templates/")
+file_name = parser.parse_args().file
 
 f = open('credentials.csv','r')
 reader = csv.reader(f,delimiter=',')
@@ -17,20 +16,22 @@ reader.next()
 key = reader.next()
 f.close()
 connection = boto.ec2.connect_to_region('us-east-1', aws_access_key_id=key[1], aws_secret_access_key=key[2])
-# key = connection.create_key_pair('topera')
-# key.save(".")
 sec_group = None
+
 try:
+    print "Trying to create security group"
     sec_group = connection.create_security_group('csc326-group30','group for users of the search engine Topera')
 
 
     connection.authorize_security_group(group_name='csc326-group30', ip_protocol='ICMP', from_port=-1, to_port=-1, cidr_ip='0.0.0.0/0')
     connection.authorize_security_group(group_name='csc326-group30', ip_protocol='TCP', from_port=22, to_port=22, cidr_ip='0.0.0.0/0')
     connection.authorize_security_group(group_name='csc326-group30', ip_protocol='TCP', from_port=80, to_port=80, cidr_ip='0.0.0.0/0')
+    print "Secutiry Group Created. Name: csc326-group30"
 except boto.exception.EC2ResponseError:
     sec_group = 'csc326-group30'
-    print "Already exist"
+    print "The Security Group is Already Created"
 
+print "Creating instance"
 res = connection.run_instances('ami-8caa1ce4', key_name="topera", security_groups=["csc326-group30"])
 ins = res.instances[0]
 while ins.state != unicode('running'):
@@ -38,55 +39,50 @@ while ins.state != unicode('running'):
     time.sleep(1)
     ins.update()
 ip = ins.ip_address
-print ip
+print "Creation Successful. IP: " + ip
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# raw_input("Wait for the machine then press any key\n")
-print "Waiting for the machine. It will take about 10 minutes"
-time.sleep(300)
-print "5 minutes left"
-time.sleep(300)
 while True:
     print "Trying to connect..."
+    time.sleep(5)
     try:
         client.connect(ip,username="ubuntu",key_filename="topera.pem", timeout=20.0)
         print "Connection Successful"
         break
     except Exception:
         print "Connection Failed"
-        answer = raw_input("Try again?[y/n]\n")
-        if answer.strip() in ["y","Y","yes"]:
-            time.sleep(5)
-            print "Trying againg" 
-        else:
-            break
+        print "Trying againg" 
+        time.sleep(5)
 
-commands = ['sudo apt-get update','sudo apt-get install python-pip python-dev build-essential', 'sudo pip install --upgrade pip', 'sudo pip install --upgrade virtualenv', 'sudo pip install bottle', 'sudo pip install --upgrade oauth2client', 'sudo pip install --upgrade google-api-python-client', 'sudo pip install beaker', 'sudo apt-get install unzip']
+print "Setting up the machine"
+commands = ['sudo apt-get update','sudo apt-get install python-pip python-dev build-essential', 'sudo pip install --upgrade pip', 'sudo pip install --upgrade virtualenv', 'sudo pip install bottle', 'sudo pip install --upgrade oauth2client', 'sudo pip install --upgrade google-api-python-client', 'sudo pip install beaker', 'sudo pip install peewee','sudo apt-get install unzip']
 for n, com in enumerate(commands):
     stdin, stdout, stderr = client.exec_command(com)
-    time.sleep(5)
     if n == 1:
+        # time.sleep(3)
         stdin.write('y\n')
         time.sleep(100)
     else:
-        time.sleep(15)
+        time.sleep(20)
     print "executed '"+com+"'"
 
 
 if __name__=="__main__":
-
-
-    # file_name = parser.parse_args().file[0]
     scp = SCPClient(client.get_transport())
+    print "Uploading Webserver"
     scp.put(file_name)
+    time.sleep(5)
     stdin, stdout, stderr = client.exec_command("unzip "+file_name)
+    print "Setting up server"
     time.sleep(10)
     stdin, stdout, stderr = client.exec_command("cd "+file_name[:-4])
     time.sleep(1)
-    stdin, stdout, stderr = client.exec_command("python topera.py")
-    print stdout.next()
+    print "Executing"
+    stdin, stdout, stderr = client.exec_command("screen -d -m sudo python server.py")
+    time.sleep(2)
+    # print stdout.next()
 
     print "connection %s" % ("ok" if connection else "failed")
     print "key pair %s" % ("ok" if key else "failed")
@@ -94,3 +90,5 @@ if __name__=="__main__":
     print "reservation %s" % ("ok" if res else "failed")
     print "instance %s" % ("ok" if ins else "failed")
     print "ip %s" % ("ok" if ip else "failed")
+    print "\n"
+    print "Website IP: "+ip
